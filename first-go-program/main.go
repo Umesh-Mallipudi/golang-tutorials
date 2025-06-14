7fcc3816403b6de9b140cbc5b4cd8c3eb0532b90
Umesh-Mallipudi <umeshnaidu00@gmail.com>
2025-06-14 18:45:37 +0530

    hello world

---
+++ first-go-program/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// this is my first program in Go Lang
+/*
+I am very excited
+to Know about Go
+and Enance my skills in go.
+*/
+
+func main() {
+	fmt.Println("Hello World.")
+}
